2a653c062d72314c91b23517de86821f3b83a4b0
roderickswinny17-byte <roderickswinny17@gmail.com>
2026-09-16 09:51:42 +0530

    Update EmployeeLifecycleTest.java

--- src/test/java/tests/EmployeeLifecycleTest.java
+++ src/test/java/tests/EmployeeLifecycleTest.java
@@ -30,24 +30,18 @@ public class EmployeeLifecycleTest extends BaseTest {
         ExtentReportManager.info("=== STEP 3: Edit Employee Information ===");
         employee.searchEmployee();
         employee.verifyEmployeeAppearsInResults();
-      //  employee.openEmployeeForEditing();
-//        employee.updateJobDetails();
-//        employee.verifyJobDetailsUpdated();
+        employee.openEmployeeForEditing();
+        employee.updateJobDetails();
+        employee.verifyJobDetailsUpdated();
 
-        // Step 4 — API Validation
-//        ExtentReportManager.info("=== STEP 4: Validate via API ===");
-//        api.establishApiSession();
-//        api.verifyEmployeeExistsViaApi();
-//        api.verifyUpdatedJobDetailsViaApi();
-
-        // Step 5 — Delete Employee
-        ExtentReportManager.info("=== STEP 5: Delete Employee ===");
+        // Step 4 — Delete Employee
+        ExtentReportManager.info("=== STEP 4: Delete Employee ===");
         employee.deleteEmployee();
-       // employee.verifyEmployeeDeleted();
-     //   api.verifyEmployeeDeletedViaApi();
+        employee.verifyEmployeeDeleted();
+        api.verifyEmployeeDeletedViaApi();
 
-        // Step 6 — Logout
-        ExtentReportManager.info("=== STEP 6: Logout ===");
+        // Step 5 — Logout
+        ExtentReportManager.info("=== STEP 5: Logout ===");
         login.logoutFromApplication();
         login.verifySessionTerminated();
     }
